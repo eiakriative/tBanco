50a481676588b30cfaf62b3143628ce1cc0aa46b
André Kanashiro <alhkanashiro@gmail.com>
2022-10-18 14:10:01 -0300

    Script terminado

--- sql/CREATE_ALTER_trabalhoFutebolBancoOsorio.sql
+++ sql/CREATE_ALTER_trabalhoFutebolBancoOsorio.sql
@@ -19,14 +19,6 @@ CREATE TABLE Clube (
   PRIMARY KEY (codigoClube)
 );
 
-CREATE TABLE Calendario_PartidaPorClube(
-  FK_codigoClube INTEGER NOT NULL,
-  FK_codigoPartida INTEGER NOT NULL,
-  FOREIGN KEY (FK_codigoClube) REFERENCES Clube (codigoClube),
-  FOREIGN KEY (FK_codigoPartida) REFERENCES Partida (codigoPartida)
-  
-);
-
 CREATE TABLE Jogador (
   nomeJogador varchar(255) NOT NULL,
   codigoClube INTEGER NOT NULL,
@@ -47,23 +39,23 @@ CREATE TABLE Estadio (
   nome VARCHAR(255) NOT null,
   cidade VARCHAR(255) NOT NULL,
   estado char(2) NOT NULL,
-  PRIMARY KEY (codigoEstadio),
-  FOREIGN KEY (codigoPartida) REFERENCES Partida (codigoPartida)
+  PRIMARY KEY (codigoEstadio)
 );
-
 
 CREATE TABLE Partida (
   codigoPartida INTEGER NOT NULL,
+  codigoEstadio INTEGER NOT NULL,
   codigoClube1 INTEGER NOT NULL, 
   codigoClube2 INTEGER NOT NULL,
   PRIMARY KEY (codigoPartida),
   FOREIGN KEY (codigoClube1) REFERENCES Clube (codigoClube),
-  FOREIGN KEY (codigoClube2) REFERENCES Clube (codigoClube)
+  FOREIGN KEY (codigoClube2) REFERENCES Clube (codigoClube),
+  FOREIGN KEY (codigoEstadio) REFERENCES Estadio (codigoEstadio)
 );
 
 CREATE TABLE Partida_golsJogador(
   codigoPartida INTEGER NOT NULL,
-  golsPorPartida INTEGER NOT NULL UNIQUE,
+  golsPorPartida INTEGER NOT NULL,
   nomeJogador varchar(255) NOT NULL,
   codigoClube INTEGER NOT NULL,
   PRIMARY KEY (codigoPartida, golsPorPartida, nomeJogador, codigoClube),
@@ -73,21 +65,36 @@ CREATE TABLE Partida_golsJogador(
   FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
 );
 
+CREATE TABLE Partida_golsJogador(
+  codigoPartida INTEGER NOT NULL,
+  golsPorPartida INTEGER NOT NULL,
+  nomeJogador varchar(255) NOT NULL,
+  codigoClube INTEGER NOT NULL,
+  PRIMARY KEY (codigoPartida, golsPorPartida, nomeJogador, codigoClube),
+  FOREIGN KEY (codigoPartida) REFERENCES Partida (codigoPartida),
+  FOREIGN KEY (golsPorPartida) REFERENCES Jogador (golsPorPartida),
+  FOREIGN KEY (nomeJogador) REFERENCES Jogador (nomeJogador),
+  FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
+);
+
+-- DROPar tabela se necessário
 DROP TABLE Estadio;
 DROP TABLE Jogador;
 DROP TABLE Clube;
 DROP TABLE Partida;
-DROP TABLE Calendario_PartidaPorClube;
 DROP TABLE Partida_golsJogador;
 DROP TABLE PartidaPorClube;
 
 
+-- Select das tabela se necessário
 SELECT * FROM Clube;
 SELECT * FROM Jogador;
 SELECT * FROM Estadio;
 SELECT * FROM Partida;
-SELECT * FROM Calendario_PartidaPorClube;
 SELECT * FROM Partida_golsJogador;
+
+-- caso deseje ordenar gols por partida em ordem decrescente. 
+SELECT * FROM Partida_golsJogador ORDER BY golsPorPartida DESC;
 
 
 
--- sql/INSERT_trabalhoFutebolBancoOsorio.sql
+++ sql/INSERT_trabalhoFutebolBancoOsorio.sql
@@ -22,34 +22,20 @@ INSERT INTO Clube (codigoClube, nome) VALUES
 INSERT INTO Jogador (nomeJogador, posicao, golsPorPartida, datanascimento, datacontratacao, idade, peso, altura, codigoClube) VALUES
 	('Joel', 'Goleiro', 0, '2000-04-22', '2022-03-01', 16, 65.0, 1.3, 1),
 	('João', 'Zagueiro', 1, '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 1),
-	('Diego', 'Zagueiro', 2, '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 1),
 	('Yago', 'Lateral', 3, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 1),
-	('Hugo', 'Lateral', 4, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 1),
 	('Matheus', 'Volante', 5, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 1),
-	('Kaio', 'Volante', 6, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 1),
-	('Pedro', 'Meia', 7, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 1),
 	('Lucas', 'Meia', 8, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 1),
 	('Luciano', 'Atacante', 9, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 1),
-	('Juan', 'Atacante', 10, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 1),
 	('Marcos', 'Goleiro', 11, '2000-04-22', '2022-03-01', 16, 65.0, 1.3, 2),
-	('Rafael', 'Zagueiro', 13,  '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 2),
-	('Fabio', 'Zagueiro', 12,  '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 2),
-	('Tiago', 'Lateral', 15, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 2),
-	('Reinaldo', 'Lateral', 14, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 2),
-	('Matheus', 'Volante', 16, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 2),
-	('Jonas', 'Volante', 19, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 2),
-	('Ricardo', 'Meia', 18, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 2),
-	('Breno', 'Meia', 17, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 2),
-	('Vinicius', 'Atacante', 20, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 2),
 	('Bruno', 'Atacante', 21, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 2);
-
-INSERT INTO Partida (codigoPartida, codigoClube1, codigoClube2) VALUES
-	(1, 1, 2),
-	(2, 3, 4),
-	(3, 2, 3),
-	(4, 4, 1),
-	(5, 1, 3),
-	(6, 4, 2);
+	
+INSERT INTO Partida (codigoPartida, codigoEstadio, codigoClube1, codigoClube2) VALUES
+	(1, 1, 1, 2),
+	(2, 2, 3, 4),
+	(3, 3, 2, 3),
+	(4, 4, 4, 1),
+	(5, 5, 1, 3),
+	(6, 6, 4, 2);
 
 INSERT INTO Estadio (codigoEstadio, codigoPartida, nome, cidade, estado) VALUES
 	(1, 1, 'Estadio do Trieste', 'Curitiba', 'PR'),
@@ -59,5 +45,53 @@ INSERT INTO Estadio (codigoEstadio, codigoPartida, nome, cidade, estado) VALUES
 	(5, 5, 'Estadio Municipal Jacy Scaff - Estadio do cafe', 'Londrina', 'PR'),
 	(6, 6, 'Estadio Os Pioneiros', 'Pato Branco', 'PR');
 
-INSERT INTO Calendario_PartidaPorClube (FK_codigoPartida, FK_codigoClube) VALUES 
-(1, 1);
+INSERT INTO Partida_golsJogador (codigoPartida, golsPorPartida, nomeJogador, codigoClube) VALUES
+	(1, 0, 'Joel' , 1),
+	(4, 3, 'Joel' , 1),
+	(5, 5, 'Joel' , 1),
+	(1, 1, 'Joao' , 1),
+	(4, 0, 'Joao' , 1),
+	(5, 1, 'Joao' , 1),
+	(1, 2, 'Yago' , 1),
+	(4, 2, 'Yago' , 1),
+	(5, 3, 'Yago' , 1),
+	(1, 1, 'Matheus' , 1),
+	(4, 1, 'Matheus' , 1),
+	(5, 1, 'Matheus' , 1),
+	(1, 2, 'Lucas' , 1),
+	(4, 1, 'Lucas' , 1),
+	(5, 2, 'Lucas' , 1),
+	(1, 3, 'Luciano' , 1),
+	(4, 2, 'Luciano' , 1),
+	(5, 1, 'Luciano' , 1),
+	(1, 0, 'Marcos' , 1),
+	(3, 0, 'marcos' , 1),
+	(6, 0, 'Marcos' , 1),
+	(1, 0, 'Bruno' , 1),
+	(3, 0, 'Bruno' , 1),
+	(6, 0, 'Bruno' , 1);
+
+
+-- DROPar tabela se necessário
+DROP TABLE Estadio;
+DROP TABLE Jogador;
+DROP TABLE Clube;
+DROP TABLE Partida;
+DROP TABLE Partida_golsJogador;
+DROP TABLE PartidaPorClube;
+
+
+-- Select das tabela se necessário
+SELECT * FROM Clube;
+SELECT * FROM Jogador;
+SELECT * FROM Estadio;
+SELECT * FROM Partida;
+SELECT * FROM Partida_golsJogador;
+
+-- caso deseje ordenar gols por partida em ordem decrescente. 
+SELECT * FROM Partida_golsJogador ORDER BY golsPorPartida DESC;
+
+
+
+
+
