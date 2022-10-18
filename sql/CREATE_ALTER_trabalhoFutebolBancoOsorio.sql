b83c85014c83d6c496504bc69d6f7d755a8bde5e
André Kanashiro <alhkanashiro@gmail.com>
2022-10-18 11:59:31 -0300

    Add relacionamentos mais aprimorados

--- sql/CREATE_ALTER_trabalhoFutebolBancoOsorio.sql
+++ sql/CREATE_ALTER_trabalhoFutebolBancoOsorio.sql
@@ -15,48 +15,79 @@ USE Projeto_trabalho_campfutebol;
 
 CREATE TABLE Clube (
   codigoClube INTEGER NOT NULL,
-  codigoEstadio INTEGER NOT NULL,
   nome varchar(100) NOT NULL,
-  Jogos INTEGER NOT NULL,
   PRIMARY KEY (codigoClube)
+);
+
+CREATE TABLE Calendario_PartidaPorClube(
+  FK_codigoClube INTEGER NOT NULL,
+  FK_codigoPartida INTEGER NOT NULL,
+  FOREIGN KEY (FK_codigoClube) REFERENCES Clube (codigoClube),
+  FOREIGN KEY (FK_codigoPartida) REFERENCES Partida (codigoPartida)
+  
 );
 
 CREATE TABLE Jogador (
   nomeJogador varchar(255) NOT NULL,
+  codigoClube INTEGER NOT NULL,
+  golsPorPartida INTEGER NOT NULL UNIQUE,
   posicao varchar(50) NOT NULL,
-  gols INTEGER DEFAULT NULL,  
   datanascimento DATE NOT NULL,
   datacontratacao DATE NOT NULL,
   idade INTEGER NOT NULL,
   peso FLOAT NOT NULL,
   altura FLOAT NOT NULL,
-  codigoClube INTEGER NOT NULL,
-  PRIMARY KEY (nomeJogador, codigoClube),
-  KEY FK_Jogador (codigoClube),
-  CONSTRAINT FK_Jogador FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
+  PRIMARY KEY (nomeJogador, codigoClube, golsPorPartida),
+  FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
 );
 
 CREATE TABLE Estadio (
   codigoEstadio INTEGER NOT NULL,
-  codigoClube INTEGER NOT NULL,
+  codigoPartida INTEGER NOT NULL,
   nome VARCHAR(255) NOT null,
   cidade VARCHAR(255) NOT NULL,
   estado char(2) NOT NULL,
   PRIMARY KEY (codigoEstadio),
-  KEY FK_Estadio (codigoClube),
-  CONSTRAINT FK_Estadio FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
+  FOREIGN KEY (codigoPartida) REFERENCES Partida (codigoPartida)
 );
+
+
+CREATE TABLE Partida (
+  codigoPartida INTEGER NOT NULL,
+  codigoClube1 INTEGER NOT NULL, 
+  codigoClube2 INTEGER NOT NULL,
+  PRIMARY KEY (codigoPartida),
+  FOREIGN KEY (codigoClube1) REFERENCES Clube (codigoClube),
+  FOREIGN KEY (codigoClube2) REFERENCES Clube (codigoClube)
+);
+
+CREATE TABLE Partida_golsJogador(
+  codigoPartida INTEGER NOT NULL,
+  golsPorPartida INTEGER NOT NULL UNIQUE,
+  nomeJogador varchar(255) NOT NULL,
+  codigoClube INTEGER NOT NULL,
+  PRIMARY KEY (codigoPartida, golsPorPartida, nomeJogador, codigoClube),
+  FOREIGN KEY (codigoPartida) REFERENCES Partida (codigoPartida),
+  FOREIGN KEY (golsPorPartida) REFERENCES Jogador (golsPorPartida),
+  FOREIGN KEY (nomeJogador) REFERENCES Jogador (nomeJogador),
+  FOREIGN KEY (codigoClube) REFERENCES Clube (codigoClube)
+);
+
 DROP TABLE Estadio;
 DROP TABLE Jogador;
 DROP TABLE Clube;
 DROP TABLE Partida;
+DROP TABLE Calendario_PartidaPorClube;
+DROP TABLE Partida_golsJogador;
+DROP TABLE PartidaPorClube;
 
 
 SELECT * FROM Clube;
-SELECT * FROM Estadio;
 SELECT * FROM Jogador;
+SELECT * FROM Estadio;
 SELECT * FROM Partida;
-
+SELECT * FROM Calendario_PartidaPorClube;
+SELECT * FROM Partida_golsJogador;
 
 
 
--- sql/INSERT_trabalhoFutebolBancoOsorio.sql
+++ sql/INSERT_trabalhoFutebolBancoOsorio.sql
@@ -13,42 +13,51 @@ CREATE DATABASE Projeto_trabalho_campfutebol;
 
 USE Projeto_trabalho_campfutebol;
 
-INSERT INTO Clube (codigoClube, codigoEstadio, nome, Jogos) VALUES
-	(1, 1, 'Trieste FC', 1),
-	(2, 2, 'SE Tanguá', 2),
-	(3, 3, 'Pavão FC', 1),
-	(4, 4, 'Futeba FC', 1);
+INSERT INTO Clube (codigoClube, nome) VALUES
+	(1, 'Trieste FC'),
+	(2, 'SE Tanguá'),
+	(3, 'Pavão FC'),
+	(4, 'Futeba FC');
 
-INSERT INTO Jogador (nomeJogador, posicao, gols, datanascimento, datacontratacao, idade, peso, altura, codigoClube) VALUES
+INSERT INTO Jogador (nomeJogador, posicao, golsPorPartida, datanascimento, datacontratacao, idade, peso, altura, codigoClube) VALUES
 	('Joel', 'Goleiro', 0, '2000-04-22', '2022-03-01', 16, 65.0, 1.3, 1),
-	('João', 'Zagueiro', 2, '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 1),
-	('Diego', 'Zagueiro', 3, '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 1),
-	('Yago', 'Lateral', 5, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 1),
-	('Hugo', 'Lateral', 6, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 1),
-	('Matheus', 'Volante', 30, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 1),
-	('Kaio', 'Volante', 30, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 1),
-	('Pedro', 'Meia', 30, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 1),
-	('Lucas', 'Meia', 30, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 1),
-	('Luciano', 'Atacante', 30, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 1),
-	('Juan', 'Atacante', 30, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 1),
-	('Marcos', 'Goleiro', 0, '2000-04-22', '2022-03-01', 16, 65.0, 1.3, 2),
-	('Rafael', 'Zagueiro', 2, '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 2),
-	('Fabio', 'Zagueiro', 3, '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 2),
-	('Tiago', 'Lateral', 5, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 2),
-	('Reinaldo', 'Lateral', 6, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 2),
-	('Matheus', 'Volante', 30, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 2),
-	('Jonas', 'Volante', 30, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 2),
-	('Ricardo', 'Meia', 30, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 2),
-	('Breno', 'Meia', 30, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 2),
-	('Vinicius', 'Atacante', 30, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 2),
-	('Bruno', 'Atacante', 30, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 2);
-    
-INSERT INTO Estadio (codigoEstadio, codigoClube, nome, cidade, estado) VALUES
-	(1, 1, 'Estadio do Trieste', 'Curitiba', 'PR'),
-	(2, 2, 'Estadio SE Trangua', 'Al. Tamandaré', 'PR'),
-	(3, 3, 'Estadio RE', 'Foz do iguaçu', 'PR'),
-	(4, 4, 'Estadio FE', 'Maringa', 'PR');
+	('João', 'Zagueiro', 1, '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 1),
+	('Diego', 'Zagueiro', 2, '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 1),
+	('Yago', 'Lateral', 3, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 1),
+	('Hugo', 'Lateral', 4, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 1),
+	('Matheus', 'Volante', 5, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 1),
+	('Kaio', 'Volante', 6, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 1),
+	('Pedro', 'Meia', 7, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 1),
+	('Lucas', 'Meia', 8, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 1),
+	('Luciano', 'Atacante', 9, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 1),
+	('Juan', 'Atacante', 10, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 1),
+	('Marcos', 'Goleiro', 11, '2000-04-22', '2022-03-01', 16, 65.0, 1.3, 2),
+	('Rafael', 'Zagueiro', 13,  '1993-02-21', '2022-03-01', 17, 53.0, 1.5, 2),
+	('Fabio', 'Zagueiro', 12,  '1991-04-22', '2022-03-01', 18, 55.0, 1.5, 2),
+	('Tiago', 'Lateral', 15, '1990-04-22', '2022-03-01', 16, 62.0, 1.2, 2),
+	('Reinaldo', 'Lateral', 14, '1991-04-22', '2022-03-01', 18, 60.0, 1.7, 2),
+	('Matheus', 'Volante', 16, '1992-04-22', '2022-03-01', 18, 60.0, 1.2, 2),
+	('Jonas', 'Volante', 19, '1995-04-22', '2022-03-01', 16, 60.0, 1.6, 2),
+	('Ricardo', 'Meia', 18, '1994-04-22', '2022-03-01', 18, 60.0, 1.4, 2),
+	('Breno', 'Meia', 17, '1992-04-22', '2022-03-01', 17, 60.0, 1.5, 2),
+	('Vinicius', 'Atacante', 20, '1996-04-22', '2022-03-01', 18, 60.0, 1.8, 2),
+	('Bruno', 'Atacante', 21, '1997-04-22', '2022-03-01', 18, 60.0, 1.9, 2);
 
 INSERT INTO Partida (codigoPartida, codigoClube1, codigoClube2) VALUES
 	(1, 1, 2),
-	(2, 3, 4);
+	(2, 3, 4),
+	(3, 2, 3),
+	(4, 4, 1),
+	(5, 1, 3),
+	(6, 4, 2);
+
+INSERT INTO Estadio (codigoEstadio, codigoPartida, nome, cidade, estado) VALUES
+	(1, 1, 'Estadio do Trieste', 'Curitiba', 'PR'),
+	(2, 2, 'Estadio Durival Britto e Silva - Vila Capanema', 'Curitiba', 'PR'),
+	(3, 3, 'Estadio Regional Willie Davids', 'Maringa', 'PR'),
+	(4, 4, 'Estadio Germano Kruger', 'Ponta Grossa', 'PR'),
+	(5, 5, 'Estadio Municipal Jacy Scaff - Estadio do cafe', 'Londrina', 'PR'),
+	(6, 6, 'Estadio Os Pioneiros', 'Pato Branco', 'PR');
+
+INSERT INTO Calendario_PartidaPorClube (FK_codigoPartida, FK_codigoClube) VALUES 
+(1, 1);
